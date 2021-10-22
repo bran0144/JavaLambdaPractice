a72972ac8684e818c369811828cdf04d2bcfb586
KatieGott <Katie.Gott@target.com>
2021-10-22 10:30:28 -0500

    first lambda expression

---
+++ src/main/java/StoreInventory.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class StoreInventory {
+  Set<String> superCycleSet = new TreeSet<>();
+  List<String> myStoreList = new ArrayList<>();
+
+  public StoreInventory() {
+    myStoreList.add("Bicycle");
+    myStoreList.add("Tricycle");
+    myStoreList.add("Scooter");
+  }
+
+  public void printStoreInventory() {
+    myStoreList.forEach(item -> {
+      System.out.println("My Store item: " + item);
+    });
+  }
+
+}
+
+
+
